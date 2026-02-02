e94435bd2770a9d21eafcbf63536f3678509ffe2
zowi <xxx@example.com>
2026-02-02 10:42:51 +0100

    Removed JUnit 3 from proyect and added last version, JUnit 5 Jupiter

--- src/test/java/com/game/MasterMindLogicTest.java
+++ src/test/java/com/game/MasterMindLogicTest.java
@@ -1,4 +1,14 @@
 package com.game;
 
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
 public class MasterMindLogicTest {
+
+    @Test
+    public void testGenerateSecret(){
+
+    }
+
 }
